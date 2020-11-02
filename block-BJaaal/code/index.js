65829f4073291122cdc10d3870f1bf532c5d43c2
Abhishek Kumar <abhivision3@gmail.com>
2020-11-02 17:55:48 +0530

    1st commit

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -2,8 +2,12 @@
 
 /* Do the following:
 - Define a variable named `username` using `const` keyword and assign your name as a value.
+
+
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
 */
+const username = "Abhishek";
+let bio = "Learning to code"
 
 let firstName = "Arya";
 let lastName = "Stark";
@@ -12,6 +16,7 @@ let lastName = "Stark";
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+alert(`I am ${username} and my bio is ${bio}`);
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
@@ -27,6 +32,7 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
+alert(`The total amount is ${[amount]}`);
 
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
